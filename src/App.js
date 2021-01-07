3e7ab8fcf4827253b108e948021a33511e5e373f
Arnob Hoque <fahimhoque>
2021-01-07 14:30:54 +0600

    minor fix

--- src/App.js
+++ src/App.js
@@ -3,13 +3,21 @@ import useFetchJobs from './useFetchJobs';
 import {Container} from 'react-bootstrap';
 
 function App() {
-  const {jobs, loading, error} = useFetchJobs()
+  const [params, setParams] = useState({})
+  const [page, setPage] = useState(1)
+  const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
   return (
-    <Container>
-      {loading && <h1>Loading..</h1>}
-      {error && <h1>Error.Try Refreshing</h1>}
-      <h1>{jobs.length}</h1>
+    <Container className="my-4">
+      <h1 className="mb-4">GitHub Jobs</h1>
+      <SearchForm params={params} onParamChange={handleParamChange} />
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
+      {loading && <h1>Loading...</h1>}
+      {error && <h1>Error. Try Refreshing.</h1>}
+      {jobs.map(job => {
+        return <Job key={job.id} job={job} />
+      })}
+      <JobsPagination page={page} setPage={setPage} hasNextPage={hasNextPage} />
     </Container>
   )
 }
