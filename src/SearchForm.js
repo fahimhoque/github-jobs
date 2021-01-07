cb1cdb380829afd288ae60031a33d5afdc90d0bb
Arnob Hoque <fahimhoque>
2021-01-07 16:49:12 +0600

    changed title name

--- src/SearchForm.js
+++ src/SearchForm.js
@@ -5,10 +5,10 @@ export default function SearchForm({ params, onParamChange }) {
   return (
     <Form className="mb-4">
       <Form.Row className="align-items-end">
-        <Form.Group as={Col}>
+        {/* <Form.Group as={Col}>
           <Form.Label>Description</Form.Label>
           <Form.Control onChange={onParamChange} value={params.description} name="description" type="text" />
-        </Form.Group>
+        </Form.Group> */}
         <Form.Group as={Col}>
           <Form.Label>Location</Form.Label>
           <Form.Control onChange={onParamChange} value={params.location} name="location" type="text" />
