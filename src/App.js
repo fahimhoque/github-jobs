ce3ccda2af6fa72845ee0fe23ffff99f980b47ab
Arnob Hoque <fahimhoque>
2021-01-07 14:39:48 +0600

    completed skeleton

--- src/App.js
+++ src/App.js
@@ -10,6 +10,14 @@ function App() {
   const [page, setPage] = useState(1)
   const { jobs, loading, error, hasNextPage } = useFetchJobs(params, page)
 
+  function handleParamChange(e) {
+    const param = e.target.name
+    const value = e.target.value
+    setPage(1)
+    setParams(prevParams => {
+      return { ...prevParams, [param]: value }
+    })
+  }
   return (
     <Container className="my-4">
       <h1 className="mb-4">GitHub Jobs</h1>
